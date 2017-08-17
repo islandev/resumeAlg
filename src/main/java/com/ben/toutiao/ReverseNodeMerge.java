653f87aeff439882a0f0d8a16405d95ccfa54398
ben <ben.wangdev@gmail.com>
2017-08-17 17:51:04 +0800

    init

---
+++ src/main/java/com/ben/toutiao/ReverseNodeMerge.java
@@ -0,0 +1,16 @@
+package com.ben.toutiao;
+
+public class ReverseNodeMerge {
+    class Node{
+        int val;
+        Node next;
+    }
+
+
+    Node reverseNode(Node head){
+
+        return  head;
+    }
+
+
+}
